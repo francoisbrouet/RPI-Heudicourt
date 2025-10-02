511607deb354f69901293154eb4c7ae01cf63857
Francois Brouet <frsbrt@gmail.com>
2025-10-02 20:08:47 +0200

    251002 20:08

--- heudicourt-solaire/webserver_plot.py
+++ heudicourt-solaire/webserver_plot.py
@@ -2,7 +2,8 @@
 
 from python_functions import *
 
-plot_logdata('./logdata.db', './solaire_graph.png')
-display_logdata('./logdata.db')
+#plot_logdata('./logdata.db', './solaire_graph.png')
+plot_logdata('/home/pi/RPI-Heudicourt/database/logdata.db', '/home/pi/RPI-Heudicourt/heudicourt-solaire/solaire_graph.png')
 
-
+#display_logdata('./logdata.db')
+display_logdata('/home/pi/RPI-Heudicourt/database/logdata.db')
