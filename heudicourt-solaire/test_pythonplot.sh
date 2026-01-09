26f5a5deb15d0d6fe39fa1989765b4b06a7d002f
Francois Brouet <frsbrt@gmail.com>
2026-01-09 16:00:05 +0100

    260109 16:00

--- heudicourt-solaire/test_pythonplot.sh
+++
@@ -1,5 +0,0 @@
-/usr/bin/python3 -u ./pythonlogger.py ../database/logdata.db 192.168.1.55 30 2
-
-# (Command line)
-# /usr/bin/python3 -u /usr/local/bin/pythonlogger /home/pi/RPI-Heudicourt/database/logdata.db 192.168.1.55 60 15 >> /home/pi/pythonlogfile 2>&1 &
-
